e27644f125e259de39ca0932fa3162e316cac9c4
aguramirez <agustinramirez58@gmail.com>
2023-08-01 17:42:02 -0300

    componente para el modal

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,4 +1,4 @@
-import { Userform } from "./components/UserForm"
+import { UserModalForm } from "./components/UserModalForm";
 import { UsersList } from "./components/UsersList"
 import { useUsers } from "./hooks/useUsers";
 
@@ -19,32 +19,13 @@ export const UsersApp = () => {
 
     return (
         <>
-            {
-
-                !visibleForm ||
-                <div className="abrir-modal animacion fadeIn">
-                    <div className="modal" style={ {display:"block"}} tabIndex="-1">
-                        <div className="modal-dialog" role="document">
-                            <div className="modal-content">
-                                <div className="modal-header">
-                                    <h5 className="modal-title">
-                                        {userSelected.id > 0 ? 'Editar' : 'Crear'} Modal Usuarios
-                                    </h5>
-
-                                </div>
-                                <div className="modal-body">
-                                    <Userform
-                                        initialUserForm={initialUserForm}
-                                        userSelected={userSelected}
-                                        handlerAddUser={handlerAddUser}
-                                        handlerCloseForm={handlerCloseForm} />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            }
+            {!visibleForm ||
+                <UserModalForm
+                    userSelected={userSelected}
+                    initialUserForm={initialUserForm}
+                    handlerAddUser={handlerAddUser}
+                    handlerCloseForm={handlerCloseForm}
+                />}
 
             <div className="container my-4">
                 <h2>Users App</h2>
