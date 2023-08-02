409af640de3374488c2c07a2b2cb9a3edd08dd64
aguramirez <agustinramirez58@gmail.com>
2023-08-01 21:13:45 -0300

    hook auth 127

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,41 +1,14 @@
-import { useReducer } from "react"
 import { LoginPage } from "./auth/pages/LoginPage"
 import { UsersPage } from "./pages/UsersPage"
-import { loginReducer } from "./auth/reducers/LoginReducer"
-import Swal from "sweetalert2"
 import { NavBar } from "./components/layout/NavBar"
+import { useAuth } from "./auth/hooks/useAuth"
 
-const initialLogin = JSON.parse(sessionStorage.getItem('login')) || {
-    isAuth: false,
-    user: undefined,
-}
+
 
 export const UsersApp = () => {
 
-    const [login, dispatch] = useReducer(loginReducer, initialLogin);
+    const {login, handlerLogin, handlerLogout} = useAuth();
 
-    const handlerLogin = ({ username, password }) => {
-        if (username === 'admin' && password === '12345') {
-            const user = { username: 'admin' }
-            dispatch({
-                type: 'login',
-                payload: user,
-            });
-            sessionStorage.setItem('login', JSON.stringify({
-                isAuth: true,
-                user: user,
-            }));
-        } else {
-            Swal.fire('Error Login', 'Username o password invalidos', 'error');
-        }
-    }
-
-    const handlerLogout = () => {
-        dispatch({
-            type: 'logout',
-        });
-        sessionStorage.removeItem('login');
-    }
     return (
         <>
             {login.isAuth ?
