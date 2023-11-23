1f079b5916bc3ff0b934c528b38746d6f4b9ad83
aguramirez <agustinramirez58@gmail.com>
2023-11-23 19:27:13 -0300

    248

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { UsersApp } from './UsersApp.jsx'
 import './styles.css'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './auth/context/AuthProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
         <UsersApp />
-      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -2,29 +2,26 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import { UsersPage } from "../pages/UsersPage"
 import { NavBar } from "../components/layout/NavBar"
 import { RegisterPage } from "../pages/RegisterPage"
-import { UserProvider } from "../context/UserProvider"
 import { AuthContext } from "../auth/context/AuthContext"
 import { useContext } from "react"
 
 export const UserRoutes = () => {
-    const {login} = useContext(AuthContext);
+    const { login } = useContext(AuthContext);
 
     return (
         <>
-            <UserProvider>
-                <NavBar />
-                <Routes>
+            <NavBar />
+            <Routes>
 
-                    <Route path="users" element={<UsersPage />} />
+                <Route path="users" element={<UsersPage />} />
 
-                    {!login.isAdmin || <>
+                {!login.isAdmin || <>
                     <Route path="users/register" element={<RegisterPage />} />
                     <Route path="users/edit/:id" element={<RegisterPage />} />
-                    </>}
-                    <Route path="/" element={<Navigate to="/users" />} />
+                </>}
+                <Route path="/" element={<Navigate to="/users" />} />
 
-                </Routes>
-            </UserProvider>
+            </Routes>
         </>
     )
 }
